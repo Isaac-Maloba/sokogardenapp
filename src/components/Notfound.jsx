a121d09ff26f8dbd2e0b52d1b04d1279eb5d2e2a
Isaac-Maloba <isaaccmaloba@gmail.com>
2026-03-13 00:44:23 +0300

    first upload

---
+++ src/components/Notfound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../css/Notfound.css'
+
+const Notfound = () => {
+  return (
+    <div className='Notfound'>
+        <h1>404</h1>
+        <h3>PAGE NOT FOUND</h3>
+        <a href="/">Return to Home Page</a>
+    </div>
+  )
+}
+
+export default Notfound
